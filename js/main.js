d576658eef2d9bc619a363bbe4756338b1619ba0
yasmin-carvalho <yasminkarolynecarvalho@gmail.com>
2021-08-10 15:31:00 -0300

    testando configuraçoes fcm

---
+++ js/main.js
@@ -0,0 +1,19 @@
+{
+
+/* ========================
+  Variables
+======================== */
+
+const FIREBASE_AUTH = firebase.auth();
+const FIREBASE_MESSAGING = firebase.messaging();
+const FIREBASE_DATABASE = firebase.database();
+
+/* ========================
+  Event Listeners
+======================== */
+
+/* ========================
+  Functions
+======================== */
+
+}
